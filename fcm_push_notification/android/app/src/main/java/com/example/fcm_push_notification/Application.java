ab100c4a19a062f39794b85f750f3de7d859bf69
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-04-22 21:54:49 +0300

    Added fcm_push_notification for firebase 9

---
+++ fcm_push_notification/android/app/src/main/java/com/example/fcm_push_notification/Application.java
@@ -0,0 +1,22 @@
+package com.example.fcm_push_notification;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService;
+import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin;
+
+public class PuttyApplication extends FlutterApplication implements PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FlutterFirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+}
+
---
+++ fcm_push_notification/android/app/src/main/kotlin/com/example/fcm_push_notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fcm_push_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
